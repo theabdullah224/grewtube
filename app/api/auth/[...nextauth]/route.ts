7859ed1972248c95d75fd304d7b4ca2604c70e44
Abdullah <theabdullah224@gmail.com>
2024-09-16 17:48:09 +0500

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from "@/models/userModel";
-import { connect } from "@/dbConfig/dbConfig";
-import { NextAuthOptions } from "next-auth";
 import { authOptions } from "./authOptions";
-interface User {
-  _id: string;
-  email: string;
-  password: string;
-  role: string; // Add role to the User interface
-}
 
-
+// Export the NextAuth handler as GET and POST
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-
-// import NextAuth from "next-auth";
-// import { authOptions } from "@/app/api/auth/nextauth-options";  // Path to your auth options file
-
-// export const GET = NextAuth(authOptions);
-// export const POST = NextAuth(authOptions);
