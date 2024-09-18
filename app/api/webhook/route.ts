1484d1dac04e2a3468ee0a09f482e37c151dbcdf
Abdullah <theabdullah224@gmail.com>
2024-09-18 16:44:37 +0500

    udpate

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-08-01',
+  apiVersion: '2024-06-20',
 });
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
