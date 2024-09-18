198dd4e79e16e418f1be231c7715bc0d2dc0ba04
Abdullah <theabdullah224@gmail.com>
2024-09-18 16:39:26 +0500

    update final 2

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Your NextAuth configuration
+import { authOptions } from "@/app/api/auth/[..nextauth]"; // Your NextAuth configuration
 
 export async function POST(request: Request) {
   try {
